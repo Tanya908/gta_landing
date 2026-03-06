b4aaf0416a9634f4f28a41441336ad8f3e5ad18d
Tetiana <tanya67722@gmail.com>
2026-03-06 20:10:57 +0200

    Implement Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import gsap from 'gsap'
 import {ScrollTrigger} from 'gsap/all'
 
+import Hero from './sections/Hero';
+import NavBar from './sections/NavBar';
+
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-        <div>
-            <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-        </div>
+        <NavBar/>
+        <Hero/>
     </main>
   )
 }
